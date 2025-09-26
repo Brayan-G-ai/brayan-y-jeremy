d14f8b2ee6c611226c77e1779ae636aaada35acb
Brayan-Jeremy <gonzalezgbrayanj@gmail.com>
2025-09-25 22:09:05 -0600

    cambio

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
-  def hola
-render html: "hola, mundo!"
-end 
+  def hola 
+    render html: "¡Hola mundo!"
+  end
+
+  def saludo 
+    render html: "¡Sean bienvenidos a mi ejemplo de Rails!"
+  end
+
+  def informacion
+    render html: "El rails es un framework de codigo abierto para el desarrollo de web en lenguaje de programacion Rub"
+  end
 end
